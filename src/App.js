7b5924cddd48ba307a3c22bfb196d1e5e9c08211
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-11-23 00:04:45 +0200

    SearchForm component added and instantiated on MovieSearchPage
    react-bootstrap configured

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import Navigator from "./Navigator";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/Pages/MovieSearchPage.jsx
+++ src/Pages/MovieSearchPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ScreenContainer from "../Components/ScreenContainer";
-// import SearchForm from "../Components/SearchForm";
+import SearchForm from "../Components/SearchForm";
 // import FavouriteMoviesContainer from "../Components/FavouriteMoviesContainer";
 
 function MovieSearchPage(props) {
   return (
     <ScreenContainer>
-      {/* <SearchForm />
-      <FavouriteMoviesContainer /> */}
+      <SearchForm />
+      {/*<FavouriteMoviesContainer /> */}
     </ScreenContainer>
   );
 }
