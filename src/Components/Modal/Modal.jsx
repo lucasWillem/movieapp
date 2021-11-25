4717556be4946736a4d37a8f3c789cbd29e475e5
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-11-25 22:19:35 +0200

    Modal layout fixed and like/unlike buttons added to add or remove movie from favourites, and also to flag the selected movie (and the modal's content) as isFavourite or not

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -3,52 +3,150 @@ import "./Modal.css";
 
 import imageNotFound from "../../assets/images/image-not-found.png";
 
+import Container from "../Container";
+
 import { useStoreState, useStoreActions } from "easy-peasy";
 
-import { Modal as BootstrapModal, Image, Row, Col } from "react-bootstrap";
+import { Modal as BootstrapModal, Image, Button } from "react-bootstrap";
+
+import { HandThumbsUp, HandThumbsUpFill } from "react-bootstrap-icons";
 
 function Modal(props) {
   const { isVisible, content } = useStoreState(
     (state) => state.modalConfiguration
   );
 
-  return (
-    <BootstrapModal show={isVisible} fullscreen={true}>
-      <div
-        style={{
-          display: "flex",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Row>
-          <Col>
-            <Image
-              src={content.Poster === "N/A" ? imageNotFound : content.Poster}
-            />
-          </Col>
-          <Col>
-            <Row>
-              <h5>{content.Title}</h5>
-              <p>Genre: {content.Genre}</p>
-              <p>Released: {content.Released}</p>
-              <p>Director: {content.Director}</p>
-              <p>Writer: {content.Writer}</p>
-              <p>Featuring: {content.Actors}</p>
-              <p>Plot: {content.Plot}</p>
-              <p>Language: {content.Language}</p>
-              <p>Metascore: {content.Metascore}</p>
-              <p>imdbRating: {content.imdbRating}</p>
-            </Row>
-          </Col>
-        </Row>
-      </div>
+  const addToFavouriteMovies = useStoreActions(
+    (actions) => actions.addToFavouriteMovies
+  );
 
-      {/* <BootstrapModal.Header closeButton>
-        <BootstrapModal.Title>{content && content.Title}</BootstrapModal.Title>
+  const toggleMovieFavouredState = useStoreActions(
+    (actions) => actions.toggleMovieFavouredState
+  );
+
+  function determineLikeButtonState() {
+    if (content.isFavourite) {
+      return (
+        <Button
+          onClick={() => {
+            addToFavouriteMovies(content);
+            toggleMovieFavouredState(content);
+          }}
+          variant="light"
+        >
+          <HandThumbsUpFill style={{ fontSize: 25 }} />
+        </Button>
+      );
+    }
+
+    if (!content.isFavourite) {
+      return (
+        <Button
+          onClick={() => {
+            addToFavouriteMovies(content);
+            toggleMovieFavouredState(content);
+          }}
+          variant="light"
+        >
+          <HandThumbsUp style={{ fontSize: 25 }} />
+        </Button>
+      );
+    }
+  }
+
+  return (
+    <BootstrapModal show={isVisible}>
+      <BootstrapModal.Header closeButton>
+        <BootstrapModal.Title>{content.Title}</BootstrapModal.Title>
       </BootstrapModal.Header>
-      <BootstrapModal.Body>Modal body content</BootstrapModal.Body> */}
+      <>
+        <Container style={{ display: "flex", justifyContent: "center" }}>
+          <Image
+            fluid
+            src={content.Poster === "N/A" ? imageNotFound : content.Poster}
+          />
+        </Container>
+
+        <BootstrapModal.Body>{content.Plot}</BootstrapModal.Body>
+        <BootstrapModal.Body>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ fontWeight: "bold", marginRight: 10 }}> Genre:</p>{" "}
+            <p>{content.Genre}</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "stretch",
+            }}
+          >
+            <p style={{ fontWeight: "bold", marginRight: 10 }}> Released on:</p>
+            <p>{content.Released}</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ fontWeight: "bold", marginRight: 10 }}> Director:</p>{" "}
+            <p>{content.Director}</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ fontWeight: "bold", marginRight: 10 }}> Writer(s):</p>{" "}
+            <p>{content.Writer}</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ fontWeight: "bold", marginRight: 10 }}> Featuring:</p>{" "}
+            <p>{content.Actors}</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ fontWeight: "bold", marginRight: 10 }}> Language(s):</p>{" "}
+            <p>{content.Language}</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ fontWeight: "bold", marginRight: 10 }}> Meta Score:</p>
+            <p>{content.Metascore}</p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <p style={{ fontWeight: "bold", marginRight: 10 }}> IMDB:</p>
+            <p>{content.imdbRating}</p>
+          </div>
+          <BootstrapModal.Footer>
+            {determineLikeButtonState()}
+          </BootstrapModal.Footer>
+        </BootstrapModal.Body>
+      </>
     </BootstrapModal>
   );
 }
