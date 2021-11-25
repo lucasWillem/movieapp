9f65dac53990c54ccd2d51d688be2db641536dec
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-11-26 01:40:01 +0200

    movie search model added

--- src/Redux/Models/index.js
+++ src/Redux/Models/index.js
@@ -2,12 +2,14 @@ import alertsModel from "./Alert.model";
 import favouriteMoviesModel from "./FavouriteMovies.model";
 import loadingDisplayModel from "./LoadingDisplay.model";
 import modalModel from "./Modal.model";
+import movieSearchModel from "./MovieSearch.model";
 
 const model = {
   alerts: alertsModel,
   favouriteMovies: favouriteMoviesModel,
   loadingDisplay: loadingDisplayModel,
   modal: modalModel,
+  movieSearch: movieSearchModel,
 };
 
 export default model;
