18adbbb911828994e0e3267891967977cbb800d5
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-11-26 02:01:19 +0200

    * Movie display variation model added * all models spread into model object

--- src/Redux/Models/index.js
+++ src/Redux/Models/index.js
@@ -4,14 +4,16 @@ import loadingDisplayModel from "./LoadingDisplay.model";
 import modalModel from "./Modal.model";
 import movieSearchModel from "./MovieSearch.model";
 import selectedMovieModel from "./SelectedMovie.model";
+import movieDisplayVariationModel from "./MovieDisplayVariation.model";
 
 const model = {
-  alerts: alertsModel,
-  favouriteMovies: favouriteMoviesModel,
-  loadingDisplay: loadingDisplayModel,
-  modal: modalModel,
-  movieSearch: movieSearchModel,
-  selectedMovie: selectedMovieModel,
+  ...alertsModel,
+  ...favouriteMoviesModel,
+  ...loadingDisplayModel,
+  ...modalModel,
+  ...movieSearchModel,
+  ...selectedMovieModel,
+  ...movieDisplayVariationModel,
 };
 
 export default model;
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,10 +1,7 @@
-import { createStore, action, thunk } from "easy-peasy";
+import { createStore } from "easy-peasy";
 
-const store = createStore({
-  movieListVariation: "card",
-  setMovieListVariation: action((state, payload) => {
-    state.movieListVariation = payload;
-  }),
-});
+import model from "./Models";
+
+const store = createStore(model);
 
 export { store };
