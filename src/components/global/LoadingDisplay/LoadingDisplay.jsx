43b2d23657c5adb80fb7db0a631832737c691a75
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-11-27 00:31:11 +0200

    loading display - styles via classnames

--- src/components/global/LoadingDisplay/LoadingDisplay.jsx
+++ src/components/global/LoadingDisplay/LoadingDisplay.jsx
@@ -7,18 +7,8 @@ import "./LoadingDisplay.css";
 function LoadingDisplay(props) {
   const isLoaderVisible = useStoreState((state) => state.isLoaderVisible);
 
-  const loadingDisplay = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "fixed",
-    top: 50,
-    left: 0,
-    right: 0,
-  };
-
   return (
-    <div style={loadingDisplay}>
+    <div className={"loading-display-wrapper"}>
       {isLoaderVisible && <Spinner animation="border" role="status" />}
     </div>
   );
