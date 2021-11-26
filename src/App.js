230b2b2e482cc2d93e408ec788f139ff82a41691
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-11-26 21:20:19 +0200

    ran pretty-quick on all files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Navigator from "./navigation/Navigator";
+import Navigator from './navigation/Navigator'
 
-import LoadingDisplay from "./components/global/LoadingDisplay";
-import Alert from "./components/global/Alert";
+import LoadingDisplay from './components/global/LoadingDisplay'
+import Alert from './components/global/Alert'
 
-require("dotenv").config();
+require('dotenv').config()
 
-function App() {
+function App () {
   return (
     <div className="App">
       <LoadingDisplay />
@@ -17,7 +18,7 @@ function App() {
         <Navigator />
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
