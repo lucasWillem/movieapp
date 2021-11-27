fe00faa20340d314b0edd1e144a6603fd7ca62ac
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-11-27 14:44:55 +0200

    useStoreRehydrated used to render app only if the store has been rehydrated, otherwise a loading display is rendered

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { useStoreRehydrated } from "easy-peasy";
+import { Spinner } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigator from "navigation/Navigator";
@@ -9,13 +11,30 @@ import Alert from "components/global/Alert";
 require("dotenv").config();
 
 function App() {
+  const storeIsRehydrated = useStoreRehydrated();
+
   return (
     <div className="App">
-      <LoadingDisplay />
-      <Alert />
-      <BrowserRouter>
-        <Navigator />
-      </BrowserRouter>
+      {storeIsRehydrated ? (
+        <>
+          <LoadingDisplay />
+          <Alert />
+          <BrowserRouter>
+            <Navigator />
+          </BrowserRouter>
+        </>
+      ) : (
+        <div
+          style={{
+            marginTop: "5%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <Spinner animation="border" role="status" />
+        </div>
+      )}
     </div>
   );
 }
