28dfa989d2b2043e79e68347777c625d05fd5799
lucasWillem <62010662+lucasWillem@users.noreply.github.com>
2021-11-27 15:24:31 +0200

    Lucas/resolving a few issues (#24)
    * duplicate search result item removed before
    resultset stored in redux store
    
    * useStoreRehydrated used to render app only
    if the store has been rehydrated,
    otherwise a loading display is rendered
    
    * *loading display removed when modal is opening
     for a specific movie.
    *easy peasy rehydrate hook implemented
    * loading display shown while store is rehydrating, app is
      rendered once store rehydration complete
    * readme updated

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Pertinent Information regarding this app
 
-Please run 'yarn install' or 'npm install' before building the app
-
-# Important observation
-
-I noticed that the API sends back a duplicate record for Avengers Endgame. Because both records share the same imdbID I was unable to remove it (with filter() for example)
+Please run 'yarn install' or 'npm install' before building the
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { useStoreRehydrated, useStoreActions, useStoreState } from "easy-peasy";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigator from "navigation/Navigator";
@@ -9,13 +10,31 @@ import Alert from "components/global/Alert";
 require("dotenv").config();
 
 function App() {
+  const storeIsRehydrated = useStoreRehydrated();
+
+  const setLoaderVisibility = useStoreActions(
+    (actions) => actions.setLoaderVisibility
+  );
+
+  const isLoaderVisible = useStoreState((state) => state.isLoaderVisible);
+
+  useEffect(() => {
+    !storeIsRehydrated ? setLoaderVisibility(true) : setLoaderVisibility(false);
+  }, [setLoaderVisibility, storeIsRehydrated]);
+
   return (
     <div className="App">
-      <LoadingDisplay />
-      <Alert />
-      <BrowserRouter>
-        <Navigator />
-      </BrowserRouter>
+      {isLoaderVisible ? (
+        <LoadingDisplay />
+      ) : (
+        <>
+          <LoadingDisplay />
+          <Alert />
+          <BrowserRouter>
+            <Navigator />
+          </BrowserRouter>
+        </>
+      )}
     </div>
   );
 }
