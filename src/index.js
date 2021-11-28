d5c66d65746e29341a7ccc4c4afd898d54626b8d
lucas van der merwe <lucas.willem.vdm@gmail.com>
2021-11-29 01:27:32 +0200

    imports from Redux and Models folder updated

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { StoreProvider } from "easy-peasy";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "redux/store";
+import { store } from "Redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
